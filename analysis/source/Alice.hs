25f69ab6fa00dafa3dd6b729ca574eebbd7c3395
Scott Fleischman <scott-fleischman@users.noreply.github.com>
2018-05-24 00:30:55 -0700

    Add test.

--- analysis/source/Alice.hs
+++ analysis/source/Alice.hs
@@ -1,17 +1,25 @@
 module Alice where
 
+import qualified Control.Lens as Lens
 import qualified Pdf.Toolbox.Document as Pdf.Document
 import qualified System.IO as IO
 
-main :: IO ()
-main = do
-  let documentPath = "AliceInWonderland.pdf"
-  ePageCount <- IO.withBinaryFile documentPath IO.ReadMode $ \handle ->
+pdfPath :: FilePath
+pdfPath = "AliceInWonderland.pdf"
+
+getPageCount :: FilePath -> IO (Either String Int)
+getPageCount path = do
+  ePageCount <- IO.withBinaryFile path IO.ReadMode $ \handle ->
     Pdf.Document.runPdfWithHandle handle Pdf.Document.knownFilters $ do
       pdf <- Pdf.Document.document
       catalog <- Pdf.Document.documentCatalog pdf
       rootNode <- Pdf.Document.catalogPageNode catalog
       Pdf.Document.pageNodeNKids rootNode
+  return (Lens.over Lens._Left show ePageCount)
+
+main :: IO ()
+main = do
+  ePageCount <- getPageCount pdfPath
   case ePageCount of
-    Left err -> print err
-    Right pageCount -> putStrLn $ show pageCount ++ " pages in " ++ documentPath
+    Left err -> putStrLn err
+    Right pageCount -> putStrLn $ show pageCount ++ " pages in " ++ pdfPath
---
+++ analysis/test/Main.hs
@@ -0,0 +1,29 @@
+{-# LANGUAGE TemplateHaskell #-}
+
+import qualified Alice
+import qualified Control.Monad
+import           Control.Monad.IO.Class (liftIO)
+import qualified Hedgehog
+import qualified System.Exit
+import qualified System.IO
+
+testPdfPath :: FilePath
+testPdfPath = "../" ++ Alice.pdfPath
+
+prop_pageCount :: Hedgehog.Property
+prop_pageCount =
+  Hedgehog.withTests 1 . Hedgehog.property $ do
+    pageCount <- liftIO (Alice.getPageCount testPdfPath)
+    Hedgehog.assert $ pageCount == Right 69
+
+tests :: IO Bool
+tests =
+  Hedgehog.checkSequential $$(Hedgehog.discover)
+
+main :: IO ()
+main = do
+  System.IO.hSetBuffering System.IO.stdout System.IO.LineBuffering
+  System.IO.hSetBuffering System.IO.stderr System.IO.LineBuffering
+
+  isSuccess <- tests
+  Control.Monad.unless isSuccess System.Exit.exitFailure
