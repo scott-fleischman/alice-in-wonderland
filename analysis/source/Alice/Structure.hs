7a06e2863ef1f3314b5d9a971292f02f73cf749e
Scott Fleischman <scott-fleischman@users.noreply.github.com>
2018-05-26 18:00:58 -0700

    WIP: parse chapters.

--- analysis/source/Alice/Structure.hs
+++ analysis/source/Alice/Structure.hs
@@ -2,14 +2,16 @@
 
 module Alice.Structure where
 
-import           Data.Sequence (Seq, ViewL((:<)), (<|), ViewR((:>)))
+import           Data.Sequence (Seq, ViewL((:<)), (<|), (|>), ViewR((:>)))
 import qualified Data.Sequence as Seq
 import           Data.Text (Text)
 import qualified Data.Text as Text
 
 newtype GutenbergPreamble = GutenbergPreamble (Seq Text) deriving Show
 newtype GutenbergPostlude = GutenbergPostlude (Seq Text) deriving Show
-newtype Title = Title (Seq Text) deriving Show
+newtype BookTitle = BookTitle (Seq Text) deriving Show
+newtype ChapterTitle = ChapterTitle Text deriving Show
+newtype ChapterContents = ChapterContents (Seq Text) deriving Show
 newtype TheEnd = TheEnd Text deriving Show
 
 data Error
@@ -18,14 +20,20 @@ data Error
   | NoTheEndFound
   | NonTheEndTextFoundAtEnd
   | NoChapterFound
+  | InvalidChapterTitle Text
   deriving Show
 
 data Body = Body
   { bodyPreamble :: GutenbergPreamble
-  , bodyTitle :: Title
-  , bodyChapters :: Seq Text
+  , bodyTitle :: BookTitle
+  , bodyChapters :: Seq Chapter
   , bodyTheEnd :: TheEnd
   , bodyPostlude :: GutenbergPostlude
+  } deriving Show
+
+data Chapter = Chapter
+  { chapterTitle :: ChapterTitle
+  , chapterContents :: ChapterContents
   } deriving Show
 
 parseBody :: Text -> Either Error Body
@@ -34,7 +42,8 @@ parseBody input = do
   (preamble, afterPreamble) <- parsePreambleFromLeft lineSequence
   (beforePostlude, postlude) <- parsePostludeFromRight afterPreamble
   (beforeTheEnd, theEnd) <- parseTheEndFromRight beforePostlude
-  (title, chapters) <- parseTitleFromLeft beforeTheEnd
+  (title, afterTitle) <- parseTitleFromLeft beforeTheEnd
+  chapters <- parseAllChaptersFromLeft afterTitle
   return
     Body
     { bodyPreamble = preamble
@@ -79,16 +88,42 @@ parseTheEndFromRight input =
     rest :> line | Text.strip line == "THE END" -> Right (rest, TheEnd line)
     _ :> _ -> Left NonTheEndTextFoundAtEnd
 
-parseTitleFromLeft :: Seq Text -> Either Error (Title, Seq Text)
-parseTitleFromLeft = go (Title Seq.empty)
+parseTitleFromLeft :: Seq Text -> Either Error (BookTitle, Seq Text)
+parseTitleFromLeft = go (BookTitle Seq.empty)
   where
-  go (Title soFar) input =
+  go (BookTitle soFar) input =
     case Seq.viewl input of
       Seq.EmptyL -> Left NoChapterFound
       line :< rest ->
         if matchesChapter line
-          then Right (Title soFar, line <| rest)
-          else go (Title (line <| soFar)) rest
+          then Right (BookTitle soFar, line <| rest)
+          else go (BookTitle (line <| soFar)) rest
+
+parseAllChaptersFromLeft :: Seq Text -> Either Error (Seq Chapter)
+parseAllChaptersFromLeft = go Seq.empty
+  where
+  go chapters input = do
+    result <- parseChapterFromLeft input
+    case result of
+      Left () -> return chapters
+      Right (title, contents, rest) -> go (chapters |> Chapter title contents) rest
+
+parseChapterFromLeft :: Seq Text -> Either Error (Either () (ChapterTitle, ChapterContents, Seq Text))
+parseChapterFromLeft fullInput =
+  case Seq.viewl fullInput of
+    Seq.EmptyL -> Right (Left ())
+    line :< rest | (Text.null . Text.strip) line -> parseChapterFromLeft rest
+    line :< rest | matchesChapter line ->
+      let (contents, moreChapters) = buildContents (ChapterContents Seq.empty) rest
+      in Right (Right (ChapterTitle line, contents, moreChapters))
+    line :< _ -> Left $ InvalidChapterTitle line
+  where
+  buildContents :: ChapterContents -> Seq Text -> (ChapterContents, Seq Text)
+  buildContents (ChapterContents soFar) input =
+    case Seq.viewl input of
+      Seq.EmptyL -> (ChapterContents soFar, Seq.empty)
+      line :< rest | matchesChapter line -> (ChapterContents soFar, line <| rest)
+      line :< rest -> (ChapterContents (line <| soFar), rest)
 
 matchesChapter :: Text -> Bool
 matchesChapter = Text.isPrefixOf "CHAPTER "
