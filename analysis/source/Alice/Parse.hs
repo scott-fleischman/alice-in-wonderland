6960153c16ddde1d1d1841121aac6d68bad3c631
Scott Fleischman <scott-fleischman@users.noreply.github.com>
2018-05-29 15:25:26 -0700

    Use pattern matching for most Sequences.

--- analysis/source/Alice/Parse.hs
+++ analysis/source/Alice/Parse.hs
@@ -5,7 +5,7 @@ module Alice.Parse where
 import           Alice.Structure
 import qualified Data.Char as Char
 import qualified Data.Foldable as Foldable
-import           Data.Sequence (Seq, ViewL((:<)), (<|), (|>), ViewR((:>)))
+import           Data.Sequence (Seq((:<|), (:|>)), ViewL((:<)), (<|), (|>), ViewR((:>)))
 import qualified Data.Sequence as Seq
 import           Data.Text (Text)
 import qualified Data.Text as Text
@@ -50,45 +50,37 @@ makeLineSequence = Seq.fromList . Text.splitOn "\r\n"
 parsePreambleFromLeft :: Seq Text -> Either Error (GutenbergPreamble, Seq Text)
 parsePreambleFromLeft = go (GutenbergPreamble Seq.empty)
   where
-  go (GutenbergPreamble soFar) input =
-    case Seq.viewl input of
-      Seq.EmptyL -> Left NoGutenbergStartFound
-      line :< rest ->
-        let newPreamble = GutenbergPreamble (line <| soFar)
-        in if line == "*** START OF THIS PROJECT GUTENBERG EBOOK ALICE’S ADVENTURES IN WONDERLAND ***"
-          then Right (newPreamble, rest)
-          else go newPreamble rest
+  go (GutenbergPreamble _) Seq.Empty = Left NoGutenbergStartFound
+  go (GutenbergPreamble soFar) (line :<| rest) =
+    let newPreamble = GutenbergPreamble (line <| soFar)
+    in if line == "*** START OF THIS PROJECT GUTENBERG EBOOK ALICE’S ADVENTURES IN WONDERLAND ***"
+      then Right (newPreamble, rest)
+      else go newPreamble rest
 
 parsePostludeFromRight :: Seq Text -> Either Error (Seq Text, GutenbergPostlude)
 parsePostludeFromRight = go (GutenbergPostlude Seq.empty)
   where
-  go (GutenbergPostlude soFar) input =
-    case Seq.viewr input of
-      Seq.EmptyR -> Left NoGutenbergEndFound
-      rest :> line ->
-        let newPostlude = GutenbergPostlude (line <| soFar)
-        in if line == "End of Project Gutenberg’s Alice’s Adventures in Wonderland, by Lewis Carroll"
-          then Right (rest, newPostlude)
-          else go newPostlude rest
+  go (GutenbergPostlude _) Seq.Empty = Left NoGutenbergEndFound
+  go (GutenbergPostlude soFar) (rest :|> line) =
+    let newPostlude = GutenbergPostlude (line <| soFar)
+    in if line == "End of Project Gutenberg’s Alice’s Adventures in Wonderland, by Lewis Carroll"
+      then Right (rest, newPostlude)
+      else go newPostlude rest
 
 parseTheEndFromRight :: Seq Text -> Either Error (Seq Text, TheEnd)
-parseTheEndFromRight input =
-  case Seq.viewr input of
-    Seq.EmptyR -> Left NoTheEndFound
-    rest :> line | (Text.null . Text.strip) line -> parseTheEndFromRight rest
-    rest :> line | Text.strip line == "THE END" -> Right (rest, TheEnd line)
-    _ :> line -> Left $ NonTheEndTextFoundAtEnd line
+parseTheEndFromRight Seq.Empty = Left NoTheEndFound
+parseTheEndFromRight (rest :|> line) | (Text.null . Text.strip) line = parseTheEndFromRight rest
+parseTheEndFromRight (rest :|> line) | Text.strip line == "THE END" = Right (rest, TheEnd line)
+parseTheEndFromRight (_ :|> line) = Left $ NonTheEndTextFoundAtEnd line
 
 parseTitleFromLeft :: Seq Text -> Either Error (BookTitle, Seq Text)
 parseTitleFromLeft = go (BookTitle Seq.empty)
   where
-  go (BookTitle soFar) input =
-    case Seq.viewl input of
-      Seq.EmptyL -> Left NoChapterFound
-      line :< rest ->
-        if matchesChapter line
-          then Right (BookTitle soFar, line <| rest)
-          else go (BookTitle (line <| soFar)) rest
+  go (BookTitle _) Seq.Empty = Left NoChapterFound
+  go (BookTitle soFar) (line :<| rest) =
+    if matchesChapter line
+      then Right (BookTitle soFar, line <| rest)
+      else go (BookTitle (line <| soFar)) rest
 
 parseAllChaptersFromLeft :: Seq Text -> Either Error (Seq Chapter)
 parseAllChaptersFromLeft = go Seq.empty
@@ -100,24 +92,21 @@ parseAllChaptersFromLeft = go Seq.empty
       Right (chapter, rest) -> go (chapters |> chapter) rest
 
 parseChapterFromLeft :: Seq Text -> Either Error (Either () (Chapter, Seq Text))
-parseChapterFromLeft fullInput =
-  case Seq.viewl fullInput of
-    Seq.EmptyL -> Right (Left ())
-    line :< rest | (Text.null . Text.strip) line -> parseChapterFromLeft rest
-    line :< rest | matchesChapter line -> do
-      (number, title) <- parseChapterHeading line
-      let (ChapterContents contents, moreChapters) = buildContents (ChapterContents Seq.empty) rest
-      paragraphSeqs <- parseParagraphSeqs contents
-      paragraphFormats <- parseParagraphFormats paragraphSeqs
-      Right (Right (Chapter number title (ChapterContents contents) paragraphFormats, moreChapters))
-    line :< _ -> Left $ InvalidChapterHeading line
-  where
-  buildContents :: ChapterContents -> Seq Text -> (ChapterContents, Seq Text)
-  buildContents (ChapterContents soFar) input =
-    case Seq.viewl input of
-      Seq.EmptyL -> (ChapterContents soFar, Seq.empty)
-      line :< rest | matchesChapter line -> (ChapterContents soFar, line <| rest)
-      line :< rest -> buildContents (ChapterContents (soFar |> line)) rest
+parseChapterFromLeft Seq.Empty = Right (Left ())
+parseChapterFromLeft (line :<| rest) | (Text.null . Text.strip) line = parseChapterFromLeft rest
+parseChapterFromLeft (line :<| rest) | matchesChapter line =
+  do
+    (number, title) <- parseChapterHeading line
+    let (ChapterContents contents, moreChapters) = buildContents (ChapterContents Seq.empty) rest
+    paragraphSeqs <- parseParagraphSeqs contents
+    paragraphFormats <- parseParagraphFormats paragraphSeqs
+    Right (Right (Chapter number title (ChapterContents contents) paragraphFormats, moreChapters))
+parseChapterFromLeft (line :<| _) = Left $ InvalidChapterHeading line
+
+buildContents :: ChapterContents -> Seq Text -> (ChapterContents, Seq Text)
+buildContents (ChapterContents soFar) Seq.Empty = (ChapterContents soFar, Seq.empty)
+buildContents (ChapterContents soFar) (line :<| rest) | matchesChapter line = (ChapterContents soFar, line <| rest)
+buildContents (ChapterContents soFar) (line :<| rest) = buildContents (ChapterContents (soFar |> line)) rest
 
 matchesChapter :: Text -> Bool
 matchesChapter = Text.isPrefixOf chapterPrefix
@@ -145,19 +134,16 @@ parseChapterHeading input = do
   Right (ChapterNumber number, ChapterTitle title)
 
 parseParagraphFromLeft :: Seq Text -> Either Error (ParagraphSeq, Seq Text)
-parseParagraphFromLeft input =
-  case Seq.viewl input of
-    Seq.EmptyL -> Left EmptyParagraph
-    line :< rest | Text.null line -> parseParagraphFromLeft rest
-    line :< rest ->
-      let (ParagraphSeq soFar, finalRest) = build (ParagraphSeq Seq.empty) rest
-      in Right (ParagraphSeq (line <| soFar), finalRest)
-  where
-  build (ParagraphSeq soFar) inputBuild =
-    case Seq.viewl inputBuild of
-      Seq.EmptyL -> (ParagraphSeq soFar, Seq.empty)
-      line :< _ | Text.null line -> (ParagraphSeq soFar, inputBuild)
-      line :< rest -> build (ParagraphSeq (soFar |> line)) rest
+parseParagraphFromLeft Seq.Empty = Left EmptyParagraph
+parseParagraphFromLeft (line :<| rest) | Text.null line = parseParagraphFromLeft rest
+parseParagraphFromLeft (line :<| rest) =
+  let (ParagraphSeq soFar, finalRest) = buildParagraph (ParagraphSeq Seq.empty) rest
+  in Right (ParagraphSeq (line <| soFar), finalRest)
+
+buildParagraph :: ParagraphSeq -> Seq Text -> (ParagraphSeq, Seq Text)
+buildParagraph (ParagraphSeq soFar) Seq.Empty = (ParagraphSeq soFar, Seq.empty)
+buildParagraph (ParagraphSeq soFar) (line :<| rest) | Text.null line = (ParagraphSeq soFar, line :<| rest)
+buildParagraph (ParagraphSeq soFar) (line :<| rest) = buildParagraph (ParagraphSeq (soFar |> line)) rest
 
 parseParagraphSeqs :: Seq Text -> Either Error (Seq ParagraphSeq)
 parseParagraphSeqs = go Seq.empty
@@ -171,44 +157,41 @@ parseParagraphSeqs = go Seq.empty
 parseParagraphFormats :: Seq ParagraphSeq -> Either Error (Seq ParagraphFormat)
 parseParagraphFormats = go Seq.empty
   where
-  go paras input =
+  go paras Seq.Empty = Right paras
+  go paras input@(ParagraphSeq paraLines :<| restParas) =
     let (starParaCount, afterStars) = getStarParaCount 0 input
     in if starParaCount == 0
       then
-          case Seq.viewl input of
-            Seq.EmptyL -> Right paras
-            ParagraphSeq paraLines :< restParas ->
-              case Seq.viewl paraLines of
-                Seq.EmptyL -> Left EmptyParagraph
-                firstLine :< restLines ->
-                  let isLaterEdition = ((== "[later editions continued as follows") . Text.strip) firstLine
-                  in if isLaterEdition
-                    then
-                      case Seq.viewr restLines of
-                        Seq.EmptyR -> Left LaterEditionEmpty
-                        beforeLastLine :> lastLine ->
-                          case Text.stripSuffix "]" lastLine of
-                            Nothing -> Left $ LaterEditionMissingCloseBracket lastLine
-                            Just strippedLastLine -> go (paras |> ParagraphFormatLaterEdition (beforeLastLine |> strippedLastLine)) restParas
-                    else
-                      let startsWithSpace = (not . Text.null) firstLine && (Char.isSpace . Text.head) firstLine
-                      in case startsWithSpace of
-                        True | ((== "CHORUS.") . Text.strip . Text.intercalate " " . Foldable.toList) paraLines ->
-                          go (paras |> ParagraphFormatChorusMarker) restParas
-                        True ->  go (paras |> ParagraphFormatIndented paraLines) restParas
-                        False ->
-                          let spacedText = (Text.intercalate " " . Foldable.toList) paraLines
-                          in go (paras |> ParagraphFormatPlain spacedText) restParas
+        case Seq.viewl paraLines of
+          Seq.EmptyL -> Left EmptyParagraph
+          firstLine :< restLines ->
+            let isLaterEdition = ((== "[later editions continued as follows") . Text.strip) firstLine
+            in if isLaterEdition
+              then
+                case Seq.viewr restLines of
+                  Seq.EmptyR -> Left LaterEditionEmpty
+                  beforeLastLine :> lastLine ->
+                    case Text.stripSuffix "]" lastLine of
+                      Nothing -> Left $ LaterEditionMissingCloseBracket lastLine
+                      Just strippedLastLine -> go (paras |> ParagraphFormatLaterEdition (beforeLastLine |> strippedLastLine)) restParas
+              else
+                let startsWithSpace = (not . Text.null) firstLine && (Char.isSpace . Text.head) firstLine
+                in case startsWithSpace of
+                  True | ((== "CHORUS.") . Text.strip . Text.intercalate " " . Foldable.toList) paraLines ->
+                    go (paras |> ParagraphFormatChorusMarker) restParas
+                  True ->  go (paras |> ParagraphFormatIndented paraLines) restParas
+                  False ->
+                    let spacedText = (Text.intercalate " " . Foldable.toList) paraLines
+                    in go (paras |> ParagraphFormatPlain spacedText) restParas
       else
         if starParaCount == 3
           then go (paras |> ParagraphFormatStarDivision) afterStars
           else Left $ InvalidStarParagraphCount starParaCount
 
-  getStarParaCount numberSoFar input =
-    case Seq.viewl input of
-      Seq.EmptyL -> (numberSoFar, Seq.empty)
-      ParagraphSeq paraLines :< rest ->
-        let paraText = (Text.concat . Foldable.toList) paraLines
-        in if Text.all (\c -> Char.isSpace c || c == '*') paraText
-          then getStarParaCount (numberSoFar + 1) rest
-          else (numberSoFar, rest)
+getStarParaCount :: Int -> Seq ParagraphSeq -> (Int, Seq ParagraphSeq)
+getStarParaCount numberSoFar Seq.Empty = (numberSoFar, Seq.empty)
+getStarParaCount numberSoFar (ParagraphSeq paraLines :<| rest) =
+  let paraText = (Text.concat . Foldable.toList) paraLines
+  in if Text.all (\c -> Char.isSpace c || c == '*') paraText
+    then getStarParaCount (numberSoFar + 1) rest
+    else (numberSoFar, rest)
