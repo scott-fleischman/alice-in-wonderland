9b212af1544f5760ae4e0d4b4ffa283fbc8a9c2f
Scott Fleischman <scott-fleischman@users.noreply.github.com>
2018-05-30 00:01:09 -0700

    Print words with puncutation in core text.

--- analysis/source/Alice.hs
+++ analysis/source/Alice.hs
@@ -6,13 +6,15 @@ import qualified Alice.Parse
 import qualified Alice.Sentence
 import qualified Alice.Structure
 import qualified Alice.TextFile
-import qualified Data.Text
-import qualified Data.Text.IO
+import qualified Data.Char as Char
+import           Data.Text (Text)
+import qualified Data.Text as Text
+import qualified Data.Text.IO as Text.IO
 
 run :: FilePath -> IO ()
 run path = do
   text <- Alice.TextFile.loadText path
-  putStrLn $ "Text length = " ++ show (Data.Text.length text)
+  putStrLn $ "Text length = " ++ show (Text.length text)
   body <-
     case Alice.Parse.parseBody text of
       Left err -> error (show err)
@@ -27,28 +29,34 @@ printChapter (Alice.Structure.Chapter number title _contents paragraphs) = do
   putStrLn $ show number ++ ". " ++ show title
   putStrLn ""
   let chapterWords = Alice.Sentence.allParagraphWords Alice.Structure.LaterEdition paragraphs
-  mapM_ Data.Text.IO.putStrLn $ fmap Alice.Structure.wordText chapterWords
+  mapM_ printIfContainsNonLetter $ fmap Alice.Structure.wordText chapterWords
   putStrLn "\n\n\n"
+
+printIfContainsNonLetter :: Text -> IO ()
+printIfContainsNonLetter input =
+  if Text.any (not . Char.isLetter) input
+    then Text.IO.putStrLn input
+    else return ()
 
 printParagraphFlat :: Alice.Structure.ParagraphFormat -> IO ()
 printParagraphFlat para = do
   let optionalText = Alice.Sentence.flattenParagraphFormat Alice.Structure.LaterEdition para
   case optionalText of
     Just text -> do
-      Data.Text.IO.putStrLn text
+      Text.IO.putStrLn text
       putStrLn ""
     Nothing -> return ()
 
 printParagraphFormat :: Alice.Structure.ParagraphFormat -> IO ()
 printParagraphFormat (Alice.Structure.ParagraphFormatPlain text) = do
-  Data.Text.IO.putStrLn text
+  Text.IO.putStrLn text
   putStrLn ""
 printParagraphFormat (Alice.Structure.ParagraphFormatIndented texts) = do
-  mapM_ Data.Text.IO.putStrLn texts
+  mapM_ Text.IO.putStrLn texts
   putStrLn ""
 printParagraphFormat (Alice.Structure.ParagraphFormatLaterEdition texts) = do
   putStrLn "  [later edition:]"
-  mapM_ Data.Text.IO.putStrLn texts
+  mapM_ Text.IO.putStrLn texts
   putStrLn ""
 printParagraphFormat Alice.Structure.ParagraphFormatStarDivision = do
   putStrLn " * * * (star divison) * * * "
